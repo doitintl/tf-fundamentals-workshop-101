b8fc95e89a866d201535a74197f298593990fa42
Patrick Paechnatz <patrick@doit-intl.com>
2021-12-17 13:09:06 +0100

    fix: lab-01 output naming issues in security-group module

--- 01-aws-vpc-and-security-groups/aws/modules/project/network/sg/outputs.tf
+++ 01-aws-vpc-and-security-groups/aws/modules/project/network/sg/outputs.tf
@@ -1,8 +1,8 @@
 
-output "sec_grp_host_webapp" {
+output "sec_grp_host_webapp_private" {
   value = aws_security_group.sec_grp_host_web_app_private.id
 }
 
-output "sec_grp_host_datadog_default" {
+output "sec_grp_host_webapp_public" {
   value = aws_security_group.sec_grp_host_web_app_public.id
 }
